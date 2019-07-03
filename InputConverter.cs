ffa1f99fda84b86845f71115779d30725ee8771f
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-07-02 20:06:38 -0400

    Add files via upload

---
+++ InputConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string textInput){
+            double convertedNumber;
+            if (!double.TryParse(textInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value.");
+            return convertedNumber;
+        }
+    }
+}
